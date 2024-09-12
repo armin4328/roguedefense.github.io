fe6be7e210c21bdadde3bc71cd4cf3cf82ebc1fd
armin4328 <168665082+armin4328@users.noreply.github.com>
2024-09-12 14:25:15 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -9,8 +9,9 @@ class MyGame extends Phaser.Scene {
 
     preload() {
         this.load.image("bg", "assets/bg.png");
-        this.load.image('mc', 'assets/character.png');
+        this.load.image('player', 'assets/character.png');
         this.load.image("ground", "assets/platform.png")
+        this.load.spritesheet('idle', 'assets/idle_animation.png', {frameWidth: 150, frameHeight: 250})
     }
 
     create() {
@@ -21,33 +22,43 @@ class MyGame extends Phaser.Scene {
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
 
         gameState.background = this.add.image(950, 470, "bg");
-
-        // Ground
+        
         const platforms = this.physics.add.staticGroup();
+
         platforms.create(700,928, "ground").setScale(6).refreshBody();
 
-        // Physics
-        gameState.character = this.physics.add.sprite(100,450,"mc")
-        gameState.character.setBounce(0.2);
-        gameState.character.setCollideWorldBounds(true)
-        this.physics.add.collider(gameState.character, platforms)
+
+        gameState.player = this.physics.add.sprite(100,450,"player")
+        gameState.player.setBounce(0.2);
+        gameState.player.setCollideWorldBounds(true)
+        this.physics.add.collider(gameState.player, platforms)
+
+        this.anims.create({
+            key: 'idle',
+            frames: this.anims.generateFrameNumbers('idle', {start: 0, end: 1}),
+            frameRate: 1,
+            repeat: -1
+        })
+
     } 
 
     update() {
-        // Player movement
+        gameState.player.anims.play('idle', true)
         if(keyA.isDown) {
-            gameState.character.setVelocityX(-160);
+            gameState.player.setVelocityX(-280);
         }
         if(keyD.isDown) {
-            gameState.character.setVelocityX(160);
+            gameState.player.setVelocityX(280);
         }
-        if(keyW.isDown) {
-            gameState.character.setVelocityY(-160);
+        if(keyW.isDown && this.player.body.touching.down) {
+            gameState.player.setVelocityY(-280);
         }
-
-        
+        if (keyW.isUp && keyA.isUp && keyD.isUp) {
+            gameState.player.setVelocityX(0);
+        }
     }
 }
+
 
 var config = {
     type: Phaser.AUTO,
