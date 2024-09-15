c3f181fdacb7188318bc00d3aa9307367fec90b8
armin4328 <168665082+armin4328@users.noreply.github.com>
2024-09-15 03:17:48 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ let keyA;
 let keyS;
 let keyD;
 let keyW;
-
+let space;
+let isPunching = false;
+let canJump = true;
+let velocity;
+let facing_left = false;
+let facing_right = true;
 const gameState = {};
 
 class MyGame extends Phaser.Scene {
@@ -10,10 +15,13 @@ class MyGame extends Phaser.Scene {
     preload() {
         this.load.image("bg", "assets/bg.png");
         this.load.image('player', 'assets/character.png');
+        this.load.image('enemy', 'assets/enemy.png');
         this.load.image("ground", "assets/platform.png")
         this.load.spritesheet('idle', 'assets/idle_animation.png', {frameWidth: 240, frameHeight: 240})
         this.load.spritesheet('right', 'assets/walk_right_animation.png', {frameWidth: 240, frameHeight: 240})
         this.load.spritesheet('left', 'assets/walk_left_animation.png', {frameWidth: 240, frameHeight: 240})
+        this.load.spritesheet('punch-right', 'assets/punch_right_animation.png', {frameWidth: 240, frameHeight: 240})
+        this.load.spritesheet('punch-left', 'assets/punch_left_animation.png', {frameWidth: 240, frameHeight: 240})
     }
 
     create() {
@@ -22,6 +30,7 @@ class MyGame extends Phaser.Scene {
         keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
         gameState.background = this.add.image(950, 470, "bg");
         
@@ -29,11 +38,12 @@ class MyGame extends Phaser.Scene {
 
         platforms.create(700,928, "ground").setScale(6).refreshBody();
 
-
         gameState.player = this.physics.add.sprite(100,450,"player")
+        gameState.enemy = this.physics.add.sprite(100,700,"enemy")
         gameState.player.setBounce(0.2);
         gameState.player.setCollideWorldBounds(true)
         this.physics.add.collider(gameState.player, platforms)
+        this.physics.add.collider(gameState.enemy, platforms)
 
         this.anims.create({
             key: 'idle',
@@ -53,25 +63,81 @@ class MyGame extends Phaser.Scene {
             frameRate: 8,
             repeat: -1
         })
+        this.anims.create({
+            key: 'punch-right',
+            frames: this.anims.generateFrameNumbers('punch-right', {start: 0, end: 3}),
+            frameRate: 8,
+        })
+        this.anims.create({
+            key: 'punch-left',
+            frames: this.anims.generateFrameNumbers('punch-left', {start: 0, end: 3}),
+            frameRate: 8,
+        })
+        gameState.player.on('animationcomplete-punch-right', function (animation) {
+            if (animation.key === 'punch-right') {
+                isPunching = false;
+                gameState.player.anims.play('idle', true);
+            }
+        });
+        gameState.player.on('animationcomplete-punch-left', function (animation) {
+            if (animation.key === 'punch-left') {
+                isPunching = false;
+                gameState.player.anims.play('idle', true);
+            }
+        });
 
     } 
-
+ 
     update() {
-    
-        if(keyA.isDown) {
-            gameState.player.setVelocityX(-380);
-            gameState.player.anims.play('left', true)
+        // player movement vvvv
+        if (!isPunching) {
+            if (keyA.isDown) {
+
+                gameState.player.setVelocityX(-380);
+                gameState.player.anims.play('left', true);
+
+                facing_left = true;
+                facing_right = false;
+
+            } else if (keyD.isDown) {
+
+                gameState.player.setVelocityX(380);
+                gameState.player.anims.play('right', true);
+
+                facing_right = true;
+                facing_left = false;
+
+            } else if (keyW.isDown && canJump) {
+
+                canJump = false;
+                gameState.player.setVelocityY(-380);
+                setTimeout(() => {
+                    canJump = true;
+                }, 2200);
+
+            } else if (space.isDown) {
+
+                if (facing_right || keyD.isDown) {
+                    gameState.player.anims.play('punch-right', true);
+                } else if (facing_left || keyA.isDown) {
+                    gameState.player.anims.play('punch-left', true);
+                }
+                gameState.player.setVelocityX(0);
+                isPunching = true;
+
+            } else if (keyW.isUp && keyA.isUp && keyD.isUp && space.isUp) {
+
+                gameState.player.anims.play('idle', true);
+                gameState.player.setVelocityX(0);
+
+            }
         }
-        if(keyD.isDown) {
-            gameState.player.setVelocityX(380);
-            gameState.player.anims.play('right', true)
-        }
-        if(keyW.isDown) {
-            gameState.player.setVelocityY(-380);
-        }
-        if (keyW.isUp && keyA.isUp && keyD.isUp) {
-            gameState.player.setVelocityX(0);
-            gameState.player.anims.play('idle', true)
+        //player movement ^^^^
+        //enemy movement vvvv
+        if (gameState.player.x > gameState.enemy.x) {
+            gameState.enemy.setVelocityX(70);
+        } else if (gameState.player.x < gameState.enemy.x)  {
+            gameState.enemy.setVelocityX(-70);
         }
     }
 }
